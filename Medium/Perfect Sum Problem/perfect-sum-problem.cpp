b552f897a415699669867dc36ac9ceb31576a8f6
Shivendra Pratap Singh <100084000+Shivendra12131@users.noreply.github.com>
2024-06-01 01:20:15 +0530

    Updated Solution - GfG to GitHub

--- Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -6,32 +6,33 @@ using namespace std;
 class Solution{
 
 	public:
-	
-const int MOD = 1e9 + 7;
-
-int perfectSum(int arr[], int n, int sum) {
-    vector<vector<int>> dp(n + 1, vector<int>(sum + 1, 0));
-
-    // Initialize dp[i][0] = 1 for all i
-    for (int i = 0; i <= n; ++i) {
-        dp[i][0] = 1;
-    }
-
-    // Fill the DP table
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 0; j <= sum; ++j) {
-            if (arr[i - 1] <= j) {
-                dp[i][j] = (dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]) % MOD;
-            } else {
-                dp[i][j] = dp[i - 1][j];
-            }
-        }
-    }
-
-    return dp[n][sum];
-}
+	int perfectSum(int arr[], int n, int sum)
+	{
+	    vector<vector<int>>dp(n+1,vector<int>(sum+1,0));
+	    int mod=1e9+7;
+	    for(int i=0;i<n+1;i++)
+	    {
+	        dp[i][0]=1;
+	    }
+	    for(int i=1;i<n+1;i++)
+	    {
+	        for(int j=0;j<sum+1;j++)
+	        {
+	            if(arr[i-1]<=j)
+	            {
+	                dp[i][j]=(dp[i-1][j-arr[i-1]]+dp[i-1][j])%mod;
+	            }
+	            else{
+	                dp[i][j]=dp[i-1][j];
+	            }
+	        }
+	    }
+	    return dp[n][sum];
+        // Your code goes here
+	}
 	  
 };
+
 
 //{ Driver Code Starts.
 int main() 
